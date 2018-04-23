6eebd4b7d35c5714c879af40cc197c3fc15c4b8b
Juan Resendiz <jresendiz27@gmail.com>
2018-04-23 13:08:23 -0500

    Adding project structure

---
+++ src/main/groovy/mx/jresendiz/poc/MainVerticle.groovy
@@ -0,0 +1,24 @@
+package mx.jresendiz.poc
+
+import groovy.util.logging.Slf4j
+import io.vertx.core.AbstractVerticle
+
+/**
+ * Created by jresendiz on 23/04/18.
+ */
+@Slf4j
+class MainVerticle extends AbstractVerticle {
+    void start() {
+        log.info("Starting verticle")
+        log.info("Starting verticle")
+        log.info("Starting verticle")
+        log.info("Starting verticle")
+        log.info("Starting verticle")
+        log.info("Starting verticle")
+        log.info("Starting verticle")
+        log.info("Starting verticle")
+        log.info("Starting verticle")
+        log.info("Starting verticle")
+        log.info("Starting verticle")
+    }
+}
