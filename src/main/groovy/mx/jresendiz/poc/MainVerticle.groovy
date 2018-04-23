2d22d185fa3f74a980b099da94ef4dd5d5c106b8
Juan Resendiz <jresendiz27@gmail.com>
2018-04-23 17:24:10 -0500

    Adding TODO to MainVerticle file

--- src/main/groovy/mx/jresendiz/poc/MainVerticle.groovy
+++ src/main/groovy/mx/jresendiz/poc/MainVerticle.groovy
@@ -1,5 +1,6 @@
 package mx.jresendiz.poc
 
+import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import io.vertx.core.AbstractVerticle
 
@@ -7,8 +8,12 @@ import io.vertx.core.AbstractVerticle
  * Created by jresendiz on 23/04/18.
  */
 @Slf4j
+@CompileStatic
 class MainVerticle extends AbstractVerticle {
     void start() {
+        // TODO Create Config Server
+        // TODO Create Rest Server
+        // TODO Register all events in the event-bus
         log.info("Starting verticle")
         log.info("Starting verticle")
     }
